b554fab0a469a4541ce1ed2779f056de833a7842
Ryan Anderson <ryan0425@msn.com>
2021-05-14 23:03:37 -0700

    set up jumbotron in jumbo

---
+++ src/components/Jumbo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Jumbo() {
+    return (
+        <div className="jumbotron jumbotron-fluid text-light jumbotron">
+            <div className="container">
+                <h1 className="display-4 text-center"> User Directory </h1>
+                <p className="lead text-center"> Select by picture or search in the box below </p>
+            </div>
+        </div>
+    );
+}
+
+export default Jumbo;
