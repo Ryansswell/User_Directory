0b0aa09ebc63bcd494bff03befedfd3944386dc7
Ryan Anderson <ryan0425@msn.com>
2021-05-14 18:13:21 -0700

    Added info into APP.js

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # User_Directory
+
+View a live demo of the app at https://ryansswell.github.io/User_Directory/
--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>React User Directory</h1>
+      <p> In progress of updating! Please come back soon</p>
+    </div>
+  );
 }
 
 export default App;
