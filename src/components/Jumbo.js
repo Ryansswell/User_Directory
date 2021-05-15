440b43d0be447a84fd2456409d6c5bd8db84fbdd
Ryan Anderson <ryan0425@msn.com>
2021-05-15 15:36:04 -0700

    updated the Jumbo text for header

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -3,7 +3,7 @@ import React from "react";
 
 function Jumbo() {
     return (
-        <div className="jumbotron jumbotron-fluid text-light jumbotron">
+        <div className="jumbotron jumbotron-fluid text-dark jumbotron">
             <div className="container">
                 <h1 className="display-4 text-center"> User Directory </h1>
                 <p className="lead text-center"> Select by picture or search in the box below </p>
