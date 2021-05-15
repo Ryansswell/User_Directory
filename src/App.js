2ff00a2fb8f3b2df67acc0a8f4127ac1f9dc898f
Ryan Anderson <ryan0425@msn.com>
2021-05-15 14:01:01 -0700

    updtated App.js and got site more functional!

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import React from "react";
-import Jumbo from "./components/Jumbo";
-import Results from "./components/Results";
-import SearchForm from "./components/SearchForm";
+
 import ResultsContainer from "./components/ResultsContainer";
 
-
 function App() {
-  return (
-    <div>
-      <h1>React User Directory</h1>
-      <p> In progress of updating! Please come back soon</p>
-    </div>
-  );
+  return <ResultsContainer />;
 }
+
+
 
 export default App;
